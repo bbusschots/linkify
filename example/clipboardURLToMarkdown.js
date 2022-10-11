15eaaa734e5e17500206676ce23e08030cf351d1
Bart Busscots <opensource@bartificer.net>
2022-10-11 15:52:17 +0100

    Fix: improved example transformers

--- example/clipboardURLToMarkdown.js
+++ example/clipboardURLToMarkdown.js
@@ -9,33 +9,38 @@ const mdTitleTpl = new linkify.LinkTemplate('[{{{text}}} — {{{uri.hostname}}}{
 linkify.registerTemplate('md-title', mdTitleTpl);
 
 // define & register custom transformers for domains that need them
-linkify.registerTransformer('intego.com', function(pData){
-    return new linkify.LinkData(pData.url(), pData.title().replace(' | The Mac Security Blog', ''));
-});
-linkify.registerTransformer('nakedsecurity.sophos.com', function(pData){
-    //console.log(pData);
-    return new linkify.LinkData(pData.url(), pData.title().replace(' – Naked Security', ''));
-});
-linkify.registerTransformer('krebsonsecurity.com', function(pData){
-    return new linkify.LinkData(pData.url(), pData.title().replace(' – Krebs on Security', ''));
-});
 linkify.registerTransformer('9to5mac.com', function(pData){
     return new linkify.LinkData(pData.url(), pData.mainHeading());
 });
 linkify.registerTransformer('bloomberg.com', function(pData){
     return new linkify.LinkData(pData.url(), pData.title().replace(' - Bloomberg', ''));
 });
-linkify.registerTransformer('wired.com', function(pData){
-    //console.log(pData);
-    return new linkify.LinkData(pData.url(), pData.mainHeading());
-});
-linkify.registerTransformer('theverge.com', function(pData){
-    //console.log(pData)
-    return new linkify.LinkData(pData.url(), pData.title().replace(/[ ][-][ ]The[ ]Verge.*$/, ''));
+linkify.registerTransformer('cultofmac.com', function(pData){
+    return new linkify.LinkData(pData.url(), pData.secondaryHeadings()[1]);
 });
 linkify.registerTransformer('daringfireball.net', function(pData){
-    //console.log(pData)
     return new linkify.LinkData(pData.url(), pData.title().replace(/^Daring Fireball:[ ]/, ''));
+});
+linkify.registerTransformer('intego.com', function(pData){
+    return new linkify.LinkData(pData.url(), pData.title().replace(' | The Mac Security Blog', ''));
+});
+linkify.registerTransformer('krebsonsecurity.com', function(pData){
+    return new linkify.LinkData(pData.url(), pData.title().replace(' – Krebs on Security', ''));
+});
+linkify.registerTransformer('macobserver.com', function(pData){
+    return new linkify.LinkData(pData.url(), pData.mainHeading());
+});
+linkify.registerTransformer('macstories.net', function(pData){
+    return new linkify.LinkData(pData.url(), pData.title().replace(' - MacStories', ''));
+});
+linkify.registerTransformer('nakedsecurity.sophos.com', function(pData){
+    return new linkify.LinkData(pData.url(), pData.title().replace(' – Naked Security', ''));
+});
+linkify.registerTransformer('theverge.com', function(pData){
+    return new linkify.LinkData(pData.url(), pData.title().replace(/[ ][-][ ]The[ ]Verge.*$/, ''));
+});
+linkify.registerTransformer('wired.com', function(pData){
+    return new linkify.LinkData(pData.url(), pData.mainHeading());
 });
 
 // read the URL from the clipboard
