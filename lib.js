7d75f604c0c913ce7616907907b72c3973d1b753
Bart Busscots <opensource@bartificer.net>
2023-07-08 16:37:59 +0100

    Feat: add utility function to strip UTM parameters

--- lib.js
+++ lib.js
@@ -723,6 +723,17 @@ module.exports.util = {
      */
     stripQueryString: function(url){
         return URI(url).query('').toString();
+    },
+
+    /**
+     * Remove UTM parameters from the query string in a URL.
+     * 
+     * @param {string} url
+     * @return {string}
+     * @see https://en.wikipedia.org/wiki/UTM_parameters
+     */
+    stripUTMParameters: function(url){
+        return URI(url).removeQuery(['utm_source', 'utm_medium', 'utm_campaign', 'utm_term', 'utm_content']).toString();
     }
 };
 
